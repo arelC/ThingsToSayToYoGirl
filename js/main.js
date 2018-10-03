e5ff43b551fca5e5b0cfa8d226e46aba584db573
arelC <arelclemente@gmail.com>
2018-10-02 22:06:05 -0400

    Added comments for clarity

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+////////// Displays the output on the HTML page
 var output = document.getElementById("main");
+////////// HTML button used to generate a random quote
 var btn_compliment = document.getElementById("btn_compliment");
-
+////////// An array that contains random compliments (should be changed later to pull data from a database)
 var compliments = [
     "My life has never been the same since I met you.",
     "Words can’t explain what a wonderful person you are.",
@@ -12,7 +14,7 @@ var compliments = [
     "Falling in love with you is the second best thing in the world because finding you was the first.",
     "I can’t wait to see you again."
 ];
-
+////////// Generates a random compliment when btn_compliment is clicked and is displayed on the HTML page
 btn_compliment.addEventListener("click", function () {
     random_compliment = Math.floor(Math.random() * (compliments.length - 0) + 0);
     output.innerHTML += compliments[random_compliment] + "<br>";
